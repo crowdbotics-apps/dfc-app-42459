0aaa711b93b443b4e2a9202c594666f3b94b882c
Crowdbotics <team@crowdbotics.com>
2023-07-27 04:33:08 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dfc-app-42459",
+  url: "https://dfc-app-42459.botics.co",
+  api: "https://dfc-app-42459.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
